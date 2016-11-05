f5de1496f54fa43006803c42fb58aafd6dc064d6
R Lindsay Vanderbeck <9rlv@queensu.ca>
2016-11-05 13:04:01 -0600

    Working first itteration.

--- A1.py
+++ A1.py
@@ -1,1 +1,17 @@
 #Assignment 1
+import urllib
+import xml.etree.ElementTree as ET
+
+data = urllib.urlopen('http://python-data.dr-chuck.net/comments_42.xml').read()
+
+tree = ET.fromstring(data)
+
+tree_lst = tree.findall('comments/comment')
+
+total = 0.0
+for item in tree_lst:
+    txt = item.find('count').text
+    num = float(txt)
+    total = total + num
+
+print total
